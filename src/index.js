e5f1dd78cc4ac5221f319091cc7a465865f61892
Haider Malik <haidermalik504@gmail.com>
2018-09-09 10:44:02 +0500

    setup project with apollo link state

--- src/index.js
+++ src/index.js
@@ -6,17 +6,29 @@ import { InMemoryCache } from 'apollo-cache-inmemory';
 import { ApolloProvider } from 'react-apollo';
 
 import App from './components/App';
+import { resolvers, defaults } from './resolvers';
 
 const cache = new InMemoryCache();
 
 //define typeDefs
+const typeDefs = `
+ type Task {
+   id: ID!
+   text: String!
+   completed: Boolean!
+ }
 
+`;
 //create apollo Clint
-
-//create a link using withClientState by providing resolvers, typeDefs, cache and defaults
-
+const client = new ApolloClient({
+	cache,
+	//create a link using withClientState by providing resolvers, typeDefs, cache and defaults
+	link: withClientState({ resolvers, defaults, typeDefs, cache })
+});
 render(
-  // wrapp app component with ApolloProvider
-    <App />,
-  document.getElementById('root'),
+	// wrapp app component with ApolloProvider
+	<ApolloProvider client={client}>
+		<App />
+	</ApolloProvider>,
+	document.getElementById('root')
 );
